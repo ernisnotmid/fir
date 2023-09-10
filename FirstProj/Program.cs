91cc8b1c5f9918d0fb221f5acdbe5e40ead9b1fa
ernisnotmid <sadretdinovernest0@gmail.com>
2023-09-10 16:55:28 +0300

    NewTask file added

--- FirstProj/Program.cs
+++ FirstProj/Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 namespace ClassWork{
  class Program {
-  static void Main(string[] args)  {
-   Console.WriteLine(@"Hello
-   
-   
-   
-   
-   
-   
-   , World!");
+  static void Main(string[] args)
+  {
+	  Console.OutputEncoding = System.Text.Encoding.UTF8;
+	  Console.WriteLine(@"Art by Joan G. Stark
+  _      _                        
+ : `.--.' ;              _....,_  
+ .'      `.      _..--'"'       `-._
+:          :_.-'"                  .`.
+:  6    6  :                     :  '.;
+:          :                      `..';
+`: .----. :'                          ;
+  `._Y _.'               '           ;
+    'U'      .'          `.         ; 
+       `:   ;`-..___       `.     .'`.
+jgs    _:   :  :    ```"''"'``.    `.  `.
+     .'     ;..'            .'       `.'`
+    `.......'              `........-'`");
+	  Console.ReadKey();
    }
  }}
